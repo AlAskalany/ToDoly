6868d24e91ec8d1bb4a8c0dc171b4e9d1ef362f5
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-10-13 19:40:41 +0200

    Changed access modifiers for methods in Command.java

--- src/com/alaskalany/todoly/commands/Command.java
+++ src/com/alaskalany/todoly/commands/Command.java
@@ -7,7 +7,6 @@ import com.alaskalany.todoly.todo.task.Task;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class Command {
@@ -29,59 +28,59 @@ public abstract class Command {
   }
 
   @NotNull
-  protected Scanner getScanner() {
+  private Scanner getScanner() {
     return new Scanner(System.in);
   }
 
-  protected String getInput(Scanner scanner) {
+  private String getInput(Scanner scanner) {
     return scanner.nextLine();
   }
 
-  protected String getInputString() {
+  String getInputString() {
     Scanner scanner = getScanner();
     return getInput(scanner);
   }
 
-  protected LocalDate getLocalDate(String input) {
+  LocalDate getLocalDate(String input) {
     DateParser dateParser = new DateParser();
     return dateParser.getDateFromString(input);
   }
 
-  protected boolean projectExists(String input) {
+  boolean projectExists(String input) {
     return taskManager.doesProjectExist(input);
   }
 
-  protected void createProject(String input) {
+  void createProject(String input) {
     taskManager.createProject(input);
   }
 
-  protected void addTaskToProject(String input, Integer taskIndex) {
+  void addTaskToProject(String input, Integer taskIndex) {
     taskManager.addTaskToProject(taskIndex, input);
   }
 
-  protected ArrayList<String> getTasksStringsByDueDate() {
+  ArrayList<String> getTasksStringsByDueDate() {
     return taskManager.getAllTasksByDueDate();
   }
 
-  protected void printTaskWithIndex(ArrayList<String> tasks, String task) {
+  void printTaskWithIndex(ArrayList<String> tasks, String task) {
     System.out.println((tasks.indexOf(task) + 1) + "- " + task);
   }
 
-  protected void printLineBreak() {
+  void printLineBreak() {
     System.out.println();
   }
 
-  protected ArrayList<Project> getProjects() {
+  ArrayList<Project> getProjects() {
     ArrayList<Project> projects;
     projects = taskManager.getAllProjects();
     return projects;
   }
 
-  protected ArrayList<String> getAllWithNoProject() {
+  ArrayList<String> getAllWithNoProject() {
     return taskManager.getAllWithNoProject();
   }
 
-  protected void printProjectTasks(Project project) {
+  void printProjectTasks(Project project) {
     String projectTitle = project.getTitle();
     print(projectTitle);
     printLineBreak();
@@ -94,16 +93,15 @@ public abstract class Command {
         });
   }
 
-  @NotNull
-  protected Integer inputToInteger(String input) {
+  @NotNull Integer inputToInteger(String input) {
     return Integer.valueOf(input);
   }
 
-  protected void addTask(String taskTitle) {
+  void addTask(String taskTitle) {
     taskManager.addTask(taskTitle);
   }
 
-  protected String promptForInput(String prompt) {
+  String promptForInput(String prompt) {
     prompt(prompt);
     return getInputString();
   }
